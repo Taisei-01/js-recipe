f0a618a8887e081f90be60ea3969453494ab725f
taisei <umekawa0707@gmail.com>
2022-04-08 12:29:14 +0900

    3が大好きwo

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -1,5 +1,4 @@
 const genkiFunction = function (number) {
-  // n を 1 から、 number まで、 値を 1 ずつ増やしながら、 for の {} の中身を繰り返す
   for (let n = 1; n <= number; n++) {
     if (n % 3 === 0 && n % 5 === 0) {
       console.log("FizzBuzz")
@@ -9,6 +8,9 @@ const genkiFunction = function (number) {
       console.log("Buzz")
     } else {
       console.log(n)
+    }
+    if (String(n).indexOf("3") > -1) {
+      console.log(n + "!!!!!!")
     }
   }
 }
