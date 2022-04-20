3adbf9f72f8c917321881382132f67990b7f5614
taisei <umekawa0707@gmail.com>
2022-04-20 16:07:11 +0900

    めも

---
+++ 04_ゼミ/week2/main.js
@@ -0,0 +1,10 @@
+const input = document.getElementById("memo-input")
+const addbutton = document.getElementById("add-button")
+//const container = document.getElementById("memo-container")
+
+addbutton.ondlick = function () {
+  //入力の読み取り
+
+  console.log(input.value)
+  //カードの追加
+}
