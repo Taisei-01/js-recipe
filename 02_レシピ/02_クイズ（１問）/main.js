977442dce0983693369e0d248a941d1b9f84b841
taisei <umekawa0707@gmail.com>
2022-04-20 13:58:37 +0900

    [クイズ（1問）]を学習しました。

---
+++ 02_レシピ/02_クイズ（１問）/main.js
@@ -0,0 +1,28 @@
+const choice1 = document.getElementById("choice-1")
+const choice2 = document.getElementById("choice-2")
+const choice3 = document.getElementById("choice-3")
+const feedback = document.getElementById("feedback")
+
+const choose = function (choiceString) {
+  if (choiceString === "ゴリアテ") {
+    feedback.textContent =
+      "残念！ゴリアテは、旧約聖書に登場するダビデに石で殺される巨人だよ。"
+  } else if (choiceString === "ゼニガメ") {
+    feedback.textContent =
+      "残念！ゼニガメは、クサガメまたはニホンイシガメの幼体だよ。"
+  } else if (choiceString === "ガニメデ") {
+    feedback.textContent = "正解！ガニメデは、木星の第三惑星だよ！"
+  }
+}
+choice1.onclick = function () {
+  // ゴリアテを選択
+  choose("ゴリアテ")
+}
+choice2.onclick = function () {
+  // ゼニガメを選択
+  choose("ゼニガメ")
+}
+choice3.onclick = function () {
+  // ガニメデを選択
+  choose("ガニメデ")
+}
