d130c9db623f265ea096470ff4130e41dfefc34d
taisei <umekawa0707@gmail.com>
2022-04-20 16:43:01 +0900

    メモ帳

--- 04_ゼミ/week2/main.js
+++ 04_ゼミ/week2/main.js
@@ -1,9 +1,17 @@
 const input = document.getElementById("memo-input")
-const addbutton = document.getElementById("add-button")
-//const container = document.getElementById("memo-container")
+const addButton = document.getElementById("add-button")
+const container = document.getElementById("memo-container")
 
-addbutton.ondlick = function () {
+addButton.onclick = function () {
   //入力の読み取り
-  console.log(input.value)
+  // console.log(input.value)
   //カードの追加
+
+  const card = document.createElement("div")
+  card.className = "card"
+  card.textContent = input.value
+  container.append(card)
+
+  //空にする
+  input.value = ""
 }
